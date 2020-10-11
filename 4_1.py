05b62d281b6705ae85f68113dfd449d6e1986cc3
HakkaMadan <72704582+HakkaMadan@users.noreply.github.com>
2020-10-11 22:50:33 +0530

    Add files via upload

---
+++ 4_1.py
@@ -0,0 +1,19 @@
+def computepay(h,r):
+    if h>40:
+        rp=h*r
+        op=1.5*r*(h-40)
+        calculated=rp+op
+        return calculated
+    else:
+        calculated=h*r
+        return calculated
+
+
+
+hrs = input("Enter Hours:")
+h=float(hrs)
+rt= input('enter rate per hour')
+r=float(r)
+
+p= computepay(h,r)
+print("Pay",p)
